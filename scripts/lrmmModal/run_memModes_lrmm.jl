c853bf39f49ca0f33d71362a17eea06237626543
Shagun Agarwal <shagun.1994@gmail.com>
2025-08-09 00:00:58 +0200

    Modal analysis code with LR in a proper function form.

---
+++ scripts/lrmmModal/run_memModes_lrmm.jl
@@ -0,0 +1,94 @@
+using DrWatson
+using Parameters
+using Gridap
+using Printf
+using WaveSpec
+using .Constants
+using MonolithicFEMVLFS.Resonator
+@quickactivate "MonolithicFEMVLFS.jl"
+
+include(srcdir("lrmmModal","memModes_lrmm_free_iter.jl"))
+
+
+# Directory for results
+resDir::String = "data/sims_202508/mem_modes_free/"
+
+
+# mfac = [0.2, 0.4, 0.5, 0.6, 0.8, 0.9, 1.0 ]
+# tfac = [0.05, 0.10, 0.25, 0.50, 0.75, 0.8]
+
+# mfac = [0.2, 0.4, 0.5, 0.6, 0.8, 0.9, 1.0 ]
+# tfac = [0.8]
+
+mfac = [0.9] 
+tfac = [0.1] 
+
+resMᵨ = [1]
+resKᵨ = [2.1*2.1*0]
+
+H0 = 10
+
+
+# Common parameters for all runs
+paramsBase = Membrane_modes.MembLR_params(
+
+  vtk_output = false,
+
+  H0 = H0, #m #still-water depth
+  Lm = 2*H0, #m
+  
+  # mfac = imfac,
+  # tfac = itfac,
+  # rS_by_ρw = rS,
+
+  nωₙ = 6, #number of natural frequencies to compute
+
+  # Iterative solution for wet natural frequencies
+  αRelax = 0.8,
+  maxIter = 20,
+
+  # initial guess for natural frequencies
+  ωn_guess = zeros(ComplexF64, 1) .+ 1.0
+)
+
+
+# Loop over all combinations of mfac, tfac and resonator parameters
+for (iresMᵨ, iresKᵨ) in zip(resMᵨ, resKᵨ)  
+
+  rS = Resonator.Single( iresMᵨ, iresKᵨ, 0.0, Point(30.0, 0.0) )
+  resonatorName = "res_m=" * @sprintf("%0.2f", iresMᵨ) *
+    "_k=" * @sprintf("%0.2f", iresKᵨ)*"_mem"
+
+  for imfac in mfac
+    for itfac in tfac
+
+      # Case directory
+      caseName = "mem_modes_ten" * @sprintf("%0.2f", itfac) *
+        "_mass" * @sprintf("%0.2f", imfac)
+
+      caseDir = resDir*caseName
+      # if( isdir(caseDir) )
+      #   rm(caseDir, recursive=true) #remove old data
+      #   @printf("Removed old data in %s\n", caseDir)
+      #   # return
+      # end
+      # mkdir(caseDir)
+      
+
+      # Update paramsBase for each run
+      params = Membrane_modes.MembLR_params(
+        paramsBase;
+
+        resDir = caseDir,
+        fileName = resonatorName,
+
+        mfac = imfac,
+        tfac = itfac,
+        rS_by_ρw = rS
+      )
+
+      # Run case
+      Membrane_modes.run_case(params)
+    end
+  end
+end
--- src/lrmmModal/memModes_lrmm_free_iter.jl
+++ src/lrmmModal/memModes_lrmm_free_iter.jl
@@ -1,6 +1,6 @@
-#module Membrane_modes
+module Membrane_modes
 
-using Revise
+using Parameters
 using Gridap
 using Plots
 using DrWatson
@@ -13,98 +13,37 @@ using Printf
 using MonolithicFEMVLFS.Resonator
 
 
-function run_case( mfac, tfac, rS )
-  
-  function run_freq(ω)
-
-    k = dispersionRelAng(H0, ω; msg=false)
-    @show ω, k
-  
-    # Weak form: ω dependent
-    k22(ϕ,w) = ∫( ∇(w)⋅∇(ϕ) )dΩ +
-        ∫( -w * im * k * ϕ )dΓin + ∫( -w * im * k * ϕ )dΓot
-  
-    c23(κ,w) = ∫( im*ω*w*κ )dΓfs 
-  
-    c32(ϕ,u) = ∫( -im*ω*u*ϕ )dΓfs 
+function run_case( params )
     
-    # Global matrices: ω dependent
-    K22 = get_matrix(AffineFEOperator( k22, l2, U_Ω, V_Ω ))
-    C23 = get_matrix(AffineFEOperator( c23, l2, U_Γκ, V_Ω ))
-    C32 = get_matrix(AffineFEOperator( c32, l3, U_Ω, V_Γκ ))
+  @unpack mfac, tfac, rS_by_ρw = params
+  @unpack order, vtk_output = params
+  @unpack H0, Lm = params
+  @unpack nωₙ = params
 
-    # Resonator matrices
-    D44 = -ω*ω * M44 + K44
-    DTot = -C14 * (Matrix(D44) \ C41)
+  @unpack resDir, fileName = params
   
-    # Solution
-    tick()
-    Mϕ = K22 - ( C23 * (Matrix(K33) \ C32) )
-    Mhat = C12 * (Mϕ \ C21)
-    Mtot = M11 + Mhat
-    tock()    
-  
-    # Eigen values
-    λ = LinearAlgebra.eigvals(Mtot\Matrix(K11 + DTot))
-    V = LinearAlgebra.eigvecs(Mtot\Matrix(K11 + DTot))      
-    # @show real.(λ[1:nωₙ])
-    # ωₙ = sqrt.(real.(λ))
-    
-    # Wrong
-    # Ur, S, Vr = svd(Mtot\Matrix(K11))    
-    # λ = reverse(S)
-    # V = reverse(Vr, dims=2)
-    # rλ = real.(λ[1:nωₙ])
-    # @show rλ    
-    # return(rλ[1:nωₙ], V[:,1:nωₙ])
+  fileName = resDir*"/"*fileName
 
-    @show sqrt.(λ[1:nωₙ])
-    return(λ[1:nωₙ], V[:,1:nωₙ])
-      
-  end
+  @show rS_by_ρw
 
-  caseName = "ten" * @sprintf("%0.2f", tfac) *"_mass" * @sprintf("%0.2f", mfac)
-  name::String = "data/sims_202508/mem_modes_free/mem_modes_"*caseName
-  order::Int = 2
-  vtk_output::Bool = true
-  filename = name*"/mem"
-
-  if( isdir(name) )
-    rm(name, recursive=true) #remove old data
-    @show "Removed old data in ", name
-    # return
-  end
-  mkdir(name)
-
-  @show rS
-
-  ρw = 1025 #kg/m3 water
-  H0 = 10 #m #still-water depth
-
-  # Membrane parameters
-  Lm = 2*H0 #m
+  # Constants
+  ρw = 1025 #kg/m3 water    
   @show g #defined in .Constants
+  
+  # Membrane parameters
   mᵨ = mfac  #mass per unit area of membrane / ρw
   Tᵨ = tfac *g*H0*H0 #T/ρw
 
-  # Excitation wave parameters
-  ω = 1.0 + im*1.0
-
-
   # Domain 
-  nx = 120
-  ny = 10
-  mesh_ry = 1.2 #Ratio for Geometric progression of eleSize
-  LΩ = 6*H0 
-  x₀ = 0.0
+  @unpack nx, ny, mesh_ry, LΩ = params
+  @unpack x₀, xm₀, xm₁ = params
   domain =  (x₀, x₀+LΩ, -H0, 0.0)
-  partition = (nx, ny)
-  xm₀ = x₀ + 2*H0
-  xm₁ = xm₀ + Lm
+  partition = (nx, ny)  
   @show Lm
   @show LΩ
   @show domain
   @show partition
+  @show mesh_ry
   @show (xm₀, xm₁)
   @show isinteger(Lm/LΩ*nx)
   @show LΩ/nx
@@ -182,14 +121,14 @@ function run_case( mfac, tfac, rS )
   labels_Ω.d_to_dface_to_entity[1][xΛmb_n2[1]] = new_entity
   add_tag!(labels_Ω, "mem_bnd", [new_entity])
 
-
-  writevtk(model, filename*"_model")
+  
   if vtk_output == true
-    writevtk(Ω,filename*"_O")
-    writevtk(Γ,filename*"_G")
-    writevtk(Γm,filename*"_Gm")  
-    writevtk(Γfs,filename*"_Gfs")
-    writevtk(Λmb,filename*"_Lmb")  
+    writevtk(model, fileName*"_model")
+    writevtk(Ω,fileName*"_O")
+    writevtk(Γ,fileName*"_G")
+    writevtk(Γm,fileName*"_Gm")  
+    writevtk(Γfs,fileName*"_Gfs")
+    writevtk(Λmb,fileName*"_Lmb")  
   end
 
 
@@ -234,17 +173,23 @@ function run_case( mfac, tfac, rS )
   U_Γq = TrialFESpace(V_Γq)
   î1 = VectorValue(1.0)
 
-  δ_p = DiracDelta(Γ, rS.XZ)
+  δ_p = DiracDelta(Γ, rS_by_ρw.XZ)
   # ----------------------End----------------------
 
   
   # Weak form: Constant matrices
   ∇ₙ(ϕ) = ∇(ϕ)⋅VectorValue(0.0,1.0)
+  
   m11(η,v) = ∫( mᵨ*v*η )dΓm
+  
   k11(η,v) = 
     ∫( v*g*η + Tᵨ*∇(v)⋅∇(η) )dΓm +  
     # ∫(- Tᵨ*v*∇(η)⋅nΛmb )dΛmb #diri
-    rS.K*δ_p( v*η ) #Resonator
+    rS_by_ρw.K * δ_p( v*η ) #Resonator
+  
+  k11Dry(η,v) = 
+    ∫( Tᵨ*∇(v)⋅∇(η) )dΓm 
+    # ∫(- Tᵨ*v*∇(η)⋅nΛmb )dΛmb #diri
 
 
   c12(ϕ,v) = ∫( v*ϕ )dΓm
@@ -253,11 +198,11 @@ function run_case( mfac, tfac, rS )
 
   k33(κ,u) = ∫( u*g*κ )dΓfs
 
-  c14(q,v) = -rS.K*δ_p( v*(q⋅î1) ) 
-  c41(η,ξ) = -rS.K*δ_p((ξ⋅î1)*η)
+  c14(q,v) = -rS_by_ρw.K * δ_p( v*(q⋅î1) ) 
+  c41(η,ξ) = -rS_by_ρw.K * δ_p((ξ⋅î1)*η)
 
-  m44(q,ξ) = rS.M*δ_p(q⋅ξ)
-  k44(q,ξ) = rS.K*δ_p(q⋅ξ)
+  m44(q,ξ) = rS_by_ρw.M * δ_p(q⋅ξ)
+  k44(q,ξ) = rS_by_ρw.K * δ_p(q⋅ξ)
 
   l1(v) = ∫( 0*v )dΓm
   l2(w) = ∫( 0*w )dΩ
@@ -268,6 +213,7 @@ function run_case( mfac, tfac, rS )
   # Global matrices: constant matrices
   M11 = get_matrix(AffineFEOperator( m11, l1, U_Γη, V_Γη ))
   K11 = get_matrix(AffineFEOperator( k11, l1, U_Γη, V_Γη ))
+  K11Dry = get_matrix(AffineFEOperator( k11Dry, l1, U_Γη, V_Γη ))
   C12 = get_matrix(AffineFEOperator( c12, l1, U_Ω, V_Γη ))
 
   C21 = get_matrix(AffineFEOperator( c21, l2, U_Γη, V_Ω ))
@@ -283,53 +229,97 @@ function run_case( mfac, tfac, rS )
   println("[MSG] Done Global matrices")
   println(K11 == transpose(K11))
 
-  #xp = range(xm₀, xm₁, size(V,2)+2)
+  #xp = range(xm₀, xm₁, size(V,2)+2)  
 
-  αRelax = 0.8
-  maxIter = 20
-  nωₙ = 6
-  da_ωₙ = zeros(ComplexF64, 1, nωₙ)
-  @show ωₙ=zeros(ComplexF64, 1, nωₙ) .+ ω
+  ## Dry Natural Frequencies
+  # --------------------Start--------------------
+  λDry = LinearAlgebra.eigvals(M11\Matrix(K11Dry))
+  VDry = LinearAlgebra.eigvecs(M11\Matrix(K11Dry))
+  ωn_dry = sqrt.(λDry[1:nωₙ])
+  da_V_dry = [ VDry[:,i] for i in [1:nωₙ;] ]
+  meffDry = 
+    diag( transpose(VDry[:,1:nωₙ]) * M11 * VDry[:,1:nωₙ] )
+  # ---------------------End---------------------
+
+
+  ## Function to run the frequency analysis 
+  #  for a given membrane mass and tension factor
+  # --------------------Start--------------------
+  function run_freq(ω)
+
+    k = dispersionRelAng(H0, ω; msg=false)
+    @show ω, k
+  
+    # Weak form: ω dependent
+    k22(ϕ,w) = ∫( ∇(w)⋅∇(ϕ) )dΩ +
+        ∫( -w * im * k * ϕ )dΓin + ∫( -w * im * k * ϕ )dΓot
+  
+    c23(κ,w) = ∫( im*ω*w*κ )dΓfs 
+  
+    c32(ϕ,u) = ∫( -im*ω*u*ϕ )dΓfs 
+    
+    # Global matrices: ω dependent
+    K22 = get_matrix(AffineFEOperator( k22, l2, U_Ω, V_Ω ))
+    C23 = get_matrix(AffineFEOperator( c23, l2, U_Γκ, V_Ω ))
+    C32 = get_matrix(AffineFEOperator( c32, l3, U_Ω, V_Γκ ))
+
+    # Resonator matrices
+    D44 = -ω*ω * M44 + K44
+    DTot = -C14 * (Matrix(D44) \ C41)
+  
+    # Solution
+    tick()
+    Mϕ = K22 - ( C23 * (Matrix(K33) \ C32) )
+    Mhat = C12 * (Mϕ \ C21)
+    Mtot = M11 + Mhat
+    tock()    
+  
+    # Eigen values
+    λ = LinearAlgebra.eigvals(Mtot\Matrix(K11 + DTot))
+    V = LinearAlgebra.eigvecs(Mtot\Matrix(K11 + DTot))      
+    # @show real.(λ[1:nωₙ])
+    # ωₙ = sqrt.(real.(λ))
+
+    meff = diag(transpose(V[:,1:nωₙ]) * Mtot * V[:,1:nωₙ])
+
+    # Wrong
+    # Ur, S, Vr = svd(Mtot\Matrix(K11))    
+    # λ = reverse(S)
+    # V = reverse(Vr, dims=2)
+    # rλ = real.(λ[1:nωₙ])
+    # @show rλ    
+    # return(rλ[1:nωₙ], V[:,1:nωₙ])
+
+    @show sqrt.(λ[1:nωₙ])
+    return(λ[1:nωₙ], V[:,1:nωₙ], meff)
+  end
+  # ---------------------End---------------------
+
+
+  ## Wet Natural Frequencies
+  @unpack αRelax, maxIter, ωn_guess = params
+  da_ωₙ = zeros(ComplexF64, nωₙ)
+  println("[MSG] Starting iterative solution for wet natural frequencies")
+  println(ωn_guess)
+  ωₙ = zeros(ComplexF64, nωₙ) .+ ωn_guess
   da_V = []
+  da_meff=[]
+  da_iter = zeros(Int, nωₙ)
 
-  # # For index=1 not looping coz ωₙ[1] = 0.0
-  # i = 1
-  # ωₙ, V = run_freq(ω)
-  # da_ωₙ[i] = ωₙ[1]
-  # push!(da_V, V[:,i])
-
+  
   for i in 1:nωₙ
     # global da_ωₙ, da_V  
     # global ωₙ, ω
-    local V, lIter
+    local V, lIter, meff
     lIter = 0    
     Δω = 1 
     ω = ωₙ[i]    
     while ((Δω > 1e-3) && (lIter < maxIter))
-      # global ω, ωₙ
       
-      # # Wrong
-      # rλ, V = run_freq(ω)
-      # ωₒ = ω      
-      # ωᵣ = sqrt(rλ[i])
-
       ωᵣ = real(ω)
-      λ, V = run_freq(ωᵣ)
+      λ, V, meff = run_freq(ωᵣ)
       ωₒ = ω      
       ω = sqrt(λ[i])
-
-      # if(i==1)
-      #   #ω = 0.2 * ωₙ[i] + 0.8*ω
-      #   ω = 0.0
-      #   Δω = 0.0
-      #   V = V*0.0
-      # if(i==4)
-      #   ω = 0.2 * ωᵣ + 0.8*ωₒ
-      #   Δω = abs(ω - ωₒ)/ωₒ
-      # else
-      #   ω = 0.5 * ωᵣ + 0.5*ωₒ
-      #   Δω = abs(ω - ωₒ)/ωₒ
-      # end      
 
       ω = αRelax * ω + (1 - αRelax) * ωₒ
       Δω = abs((ω - ωₒ)/ωₒ)
@@ -337,45 +327,83 @@ function run_case( mfac, tfac, rS )
       # @show ωₙ
       @show i, ω, Δω, lIter
     end    
-    da_ωₙ[i] = ω
-    push!(da_V, V[:,i])
+    da_ωₙ[i] = ω    
+    da_iter[i] = lIter
+    push!(da_V, V[:,i]) 
+    push!(da_meff, meff[i])
   end
 
-  println(da_ωₙ)
 
-  xp = range(xm₀, xm₁, length(da_V[1]))
+
+  dfWet = DataFrame(
+    ωn = da_ωₙ,
+    V = da_V,
+    meff = da_meff,
+    iter = da_iter
+  )
+  @show dfWet
+
+  dfDry = DataFrame(
+    ωn = ωn_dry,
+    V = da_V_dry,
+    meff = meffDry
+  )
+  @show dfDry  
 
   data = Dict(
-    "xp" => xp,
-    "ωₙ" => da_ωₙ,
-    "V" => da_V  
+    "params" => params,
+    "dfDry" => dfDry,
+    "dfWet" => dfWet
   )
 
-  wsave(filename*"_modesdata.jld2", data)
-end
-
-# mfac = [0.2, 0.4, 0.5, 0.6, 0.8, 0.9, 1.0 ]
-# tfac = [0.05, 0.10, 0.25, 0.50, 0.75, 0.8]
-
-# mfac = [0.2, 0.4, 0.5, 0.6, 0.8, 0.9, 1.0 ]
-# tfac = [0.8]
-
-mfac = [0.9]
-tfac = [0.1]
-
-resMᵨ = [1]
-resKᵨ = [2.1*2.1*1]
-
-for (iresMᵨ, iresKᵨ) in zip(resMᵨ, resKᵨ)  
-
-  rS = Resonator.Single( iresMᵨ, iresKᵨ, 0.0, Point(30.0, 0.0) )
-
-  for imfac in mfac
-    for itfac in tfac
-      run_case(imfac, itfac, rS)
-    end
-  end
+  wsave(fileName*"_modesdata.jld2", data)
 end
 
 
-#end
+"""
+Memb_params
+
+Parameters for the VIV.jl module.
+"""
+@with_kw struct MembLR_params
+
+  resDir::String = "data/sims_202508/mem_modes_free/"
+  fileName::String = "mem"
+
+  order::Int = 2
+  vtk_output::Bool = true  
+
+  H0 = 10 #m #still-water depth
+  Lm = 2*H0 #m
+
+  # Domain 
+  nx = 120
+  ny = 10
+  mesh_ry = 1.2 #Ratio for Geometric progression of eleSize
+  LΩ = 6*H0 
+  x₀ = 0.0
+  xm₀ = x₀ + 2*H0
+  xm₁ = xm₀ + Lm
+
+  mfac = 0.9
+  tfac = 0.1
+
+  rS_by_ρw = Resonator.Single( 1, 0.0, 0.0, Point(3*H0, 0.0) )
+
+  # Number of natural frequencies
+  nωₙ = 6
+
+  # Iterative solution for wet natural frequencies
+  αRelax = 0.8
+  maxIter = 20
+  
+  # initial guess for natural frequencies
+  ωn_guess = zeros(ComplexF64, nωₙ) .+ 1.0
+  
+  
+end
+
+
+
+
+end
